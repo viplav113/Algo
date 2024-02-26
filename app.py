f81e73f6e6254388912c1538e942c33df6da0e78
viplav113 <viplavpatwardhan777@gmail.com>
2024-02-26 22:35:11 +0530

    first commit

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def get_messages():
     return jsonify({'messages': Fskt.get_messages()})  # Example, adjust accordingly
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
